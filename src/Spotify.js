b354349145da50e712ba4f4593026a47057aa041
jonestly-source <45253980+jonestly-source@users.noreply.github.com>
2023-02-18 23:02:03 +0800

    update src/Spotify.js

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,89 @@
-import React from 'react'
+import axios from "axios";
 
-export default function Spotify() {
-  return (
-    <div>Spotify</div>
-  )
+var token = null;
+export function theToken(val) {
+  token = val;
 }
+
+export async function getRecentlyPlayed() {
+  const { data } = await axios.get(
+    "https://api.spotify.com/v1/me/player/recently-played",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        limit: 6,
+      },
+    }
+  );
+
+  if (data) {
+    const songDetails = data.items.map((track) => {
+      return {
+        title: track.track.name,
+        uri: track.track.uri,
+        albumUrl: track.track.album.images[0].url,
+      };
+    });
+    return songDetails;
+  }
+}
+
+export async function getNewRelease() {
+  const { data } = await axios.get(
+    "https://api.spotify.com/v1/browse/new-releases",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        limit: 15,
+        country: "PH",
+      },
+    }
+  );
+  if (data) {
+    const songDetails = data.albums.items.map((e) => {
+      return {
+        title: e.name,
+        uri: e.uri,
+        albumUrl: e.images[0].url,
+        albumType: e.album_type,
+        artists: e.artists[0].external_urls.name,
+      };
+    });
+    return songDetails;
+  }
+}
+
+export async function getUser() {
+  const { data } = await axios.get("https://api.spotify.com/v1/me", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return data;
+}
+
+export const getPlaybackState = async () => {
+  const { data } = await axios.get("https://api.spotify.com/v1/me/player", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return data;
+};
+
+export const getCurrentlyPlaying = async () => {
+  const { data } = await axios.get(
+    "https://api.spotify.com/v1/me/player/curreny-playing",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return data;
+};
