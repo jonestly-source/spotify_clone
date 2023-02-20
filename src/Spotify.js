350a54b27351aabd5cccec9146a0db67697f7db8
jonestly-source <marjones.lapinig@gmail.com>
2023-02-20 14:35:37 +0800

    Player

--- src/Spotify.js
+++ src/Spotify.js
@@ -24,7 +24,7 @@ export async function getRecentlyPlayed() {
         title: track.track.name,
         uri: track.track.uri,
         albumUrl: track.track.album.images[0].url,
-        artists: [track.track.artists.map((e) => e.name)],
+        artists: [track.track.artists.map((data) => data.name)],
       };
     });
     return songDetails;
@@ -46,13 +46,13 @@ export async function getNewRelease() {
   );
   if (data) {
     // console.log(data.albums.items)
-    const songDetails = data.albums.items.map((e) => {
+    const songDetails = data.albums.items.map((data) => {
       return {
-        title: e.name,
-        uri: e.uri,
-        albumUrl: e.images[0].url,
-        albumType: e.album_type,
-        artists: [e.artists.map((e) => e.name)],
+        title: data.name,
+        uri: data.uri,
+        albumUrl: data.images[0].url,
+        albumType: data.album_type,
+        artists: [data.artists.map((data) => data.name)],
       };
     });
     return songDetails;
@@ -79,7 +79,7 @@ export const getPlaybackState = async () => {
 
 export const getNowPlaying = async () => {
   const { data } = await axios.get(
-    "https://api.spotify.com/v1/me/player/curreny-playing",
+    "https://api.spotify.com/v1/me/player/currently-playing",
     {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -88,19 +88,19 @@ export const getNowPlaying = async () => {
   );
 
   if (data) {
-    return data.map((e) => {
-      return {
-        title: e.item.name,
-        uri: e.item.uri,
-        albumUrl: e.item.album.images[0].url,
-        artists: [e.artists.map((e) => e.name)],
-        playingType: e.currently_playing_type,
-        duration: e.item.duration_ms,
-        shuffleState: e.shuffle_state,
-        repeatState: e.repeat_state,
-      };
-    });
+    const songDetails = {
+      title: data.item.name,
+      uri: data.item.uri,
+      albumUrl: data.item.album.images[0].url,
+      artists: [data.item.artists.map((data) => data.name)],
+      playingType: data.currently_playing_type,
+      duration: data.item.duration_ms,
+      isPLaying: data.isPlaying,
+      progress: data.progress_ms
+    }
+    return songDetails;
   }
+  return null
 };
 
 export function msToTime(ms) {
