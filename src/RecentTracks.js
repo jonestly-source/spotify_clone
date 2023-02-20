1f2c89c4adfc55b84bdc047e4ca8bbb1ac96f68e
jonestly-source <45253980+jonestly-source@users.noreply.github.com>
2023-02-21 02:03:25 +0800

    update src/RecentTracks.js

--- src/RecentTracks.js
+++ src/RecentTracks.js
@@ -1,13 +1,13 @@
 export default function RecentTracks({ track }) {
   return (
-    <div className='tiles'>
-      <div className='cv-image'>
+    <div className="tiles">
+      <div className="cv-image">
         <img src={track.albumUrl} alt={track.name} />
       </div>
       <div className="song-details">
-        <div className='category-title'>{track.title}</div>
+        <div className="category-title">{track.title}</div>
         <div className="category-artists">{track.artists}</div>
       </div>
     </div>
-  )
+  );
 }
